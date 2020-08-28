a88a2493577770494fab27b3a376756eecf237c3
Danil <64682978+Milenium-Cr@users.noreply.github.com>
2020-08-28 22:45:47 +0300

    Added super-user and match history
    Super user is temporary solution

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ class Player:
         self.winstreak = 0
         self.bet = 0
         self.rate = 0
+        
+        # супер-юзер
         self.gm = 0
+        self.status = ""
 
         # переменные для "монеточки"
         self.coin = 0
         self.buyHelp = None
+        self.CFgames = []
+        self.CFlist_status = ""
 
     def coinflip(self):
         coin = 0
         print("Коинфлип (или же монеточка) - все зависит от вашей удачи.")
-        print("Поставьте ставку, и если вы победите, вы получите в 2 раза больше.")
+        print("Поставьте ставку, и если вы победите, вы получите в 2 раза больше от суммы ставки.")
         print("Если проиграете, вы потеряете ту сумму, которую вложили.")
         while True:
             print("\nВаша ставка? {%s}" % self.money)
@@ -24,6 +29,8 @@ class Player:
             if self.rate > self.money:
                 print("У вас мало денег для такой ставки!")
             else:
+                if self.rate == self.money:
+                    print("Видимо, вы очень смелый человек, раз решили потратить сразу все деньги..")
                 self.bet = int(input("\nОрел или решка? (1/2)\n>>> "))
                 self.coin = randint(1, 2)
                 if self.coin == self.bet:
@@ -36,6 +43,7 @@ class Player:
                     
                     print(f"Вы победили! Ваш приз - {self.rate * 2}.\nВаш баланс - {self.money}.\n")
                     print(f"Ваш винстрик - {self.winstreak}")
+                    self.CFgames.append("(+%(win)s) {Баланс %(money)s}" % {"win": self.rate, "money": self.money})
                 else:
                     print("К сожалению, вы проиграли.")
                     if self.winstreak >= 10:
@@ -46,6 +54,9 @@ class Player:
                             self.winstreak -= 10
                             print("Вы не потратили свои деньги.")
                             print(f"Ваш винстрик - {self.winstreak}")
+                            self.CFgames.append("(Купил 2 шанс) {Баланс %s}" % self.money)
+                        else:
+                            self.buyHelp = False
                     else:
                         self.buyHelp = False
                             
@@ -59,6 +70,7 @@ class Player:
                         elif self.winstreak > 0:
                             self.winstreak = 0
                         print(f"Ваш винстрик - {self.winstreak}")
+                        self.CFgames.append("(-%(lose)s) {Баланс %(money)s}" % {"lose": self.rate, "money": self.money})
                     if self.money > 0:
                         print("Сыграем еще раз? (y/n)")
                         i = input(">>> ")
@@ -74,7 +86,43 @@ class Player:
             coin = 0
             self.bet = 0
             self.rate = 0
+    
+    def op(self):
+        self.gm = 1
+        self.money = 1000000
+        self.winstreak = 5000
+        self.status = "[АКТИВИРОВАНО]"
+        print("Super-User активирован!")
+    
+    # история coinflip игр
+    def CFreplays(self):
+        if self.CFgames == []:
+            print("Неактивирован. Сыграйте 1 игру в CF.")
+        else:
+            self.CFlist_status = "[Активно!]"
+            count = 0
+            for j in self.CFgames:
+                count += 1
+                print(f"{count}. {j}")
 
 
 user = Player()
-user.coinflip()
+while True:
+    print("Выберите функцию")
+    print("1. Коинфлип")
+    print("2. Права супер-юзера %s" % user.status)
+    print("3. История игр %s" % user.CFlist_status)
+    print("4. Баланс игрока")
+    
+    act = int(input(">>> "))
+    
+    if act == 1:
+        user.coinflip()
+    elif act == 2:
+        user.op()
+    elif act == 3:
+        user.CFreplays()
+    elif act == 4:
+        print(f"Баланс - {user.money}")
+    else:
+        break
